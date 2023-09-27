9e1b195b57e79fa8ac9060495ffb73b4f98100de
amitsharma21 <9amithp@gmail.com>
2023-09-27 13:34:08 +0530

    changes

--- Bash/bash_script.sh
+++ Bash/bash_script.sh
@@ -1,14 +1,3 @@
 #! /bin/bash
+echo "amit sharma"
 
-read -p "do you like programming? " ans
-case $ans in
-	Yes|yes|y|Y)
-		echo "yohooo he likes programming"
-		;;
-	No|no|n|N)
-		echo "oops he dont likes programming"
-		;;
-	*)
-		echo "wrong input"
-		;;
-esac
